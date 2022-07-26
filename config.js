a4eb3fcafd3c7e79b73336da6ca3ce5f35e2c8a0
MFXZ <109869000+MFXZ@users.noreply.github.com>
2022-07-26 23:57:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -43,26 +43,26 @@ global.sdc = 'https://youtube.com/channel/UCACHvReRmw2fxgMutPFCBWg' //Discord Mu
 global.snh = 'https://nhentai.net/g/365296/' //Make ini ana gausah di ganti.
 
 /*⫘⫘⫘⫘ALL PAYMENT⫘⫘⫘⫘⫘*/
-global.pdana = '088215689772' //Dana
-global.povo = '088215689772' //Ovo
-global.pgopay = '088215689772' //Gopay
-global.ppulsa = '088215689772' //Pulsa 
-global.ppulsa2 = '0895394988123' //Pulsa, Kalau nomor cuma 1, yg ini kasih tanda -
-global.psaweria = 'https://saweria.co/FangzBot' //Saweria
+global.pdana = '' //Dana
+global.povo = '' //Ovo
+global.pgopay = '' //Gopay
+global.ppulsa = '' //Pulsa 
+global.ppulsa2 = '' //Pulsa, Kalau nomor cuma 1, yg ini kasih tanda -
+global.psaweria = '' //Saweria
 
 /*⫘⫘⫘⫘ GLOBAL NOMOR ⫘⫘⫘⫘⫘*/
-global.nomorbot = '62895394988123' //Nomor Bot mu
-global.nomorown = '6288215689772' //Nomormu
-global.namebot = 'Fangz BOT' //Nama bot mu
-global.nameown = 'Fangz Ganz' //Nama mu
+global.nomorbot = '6288983514031' //Nomor Bot mu
+global.nomorown = '628981614707' //Nomormu
+global.namebot = 'MFC BOTZ' //Nama bot mu
+global.nameown = 'MFX' //Nama mu
 
 
 /*⫘⫘⫘⫘ STAFF ⫘⫘⫘⫘⫘*/
 global.owner = [
-  ['6288215689772'], //Owner1
-  ['6288215689772'], //Owner2
-  ['6288215689772'], //Owner3
-  ['6288215689772', 'FANGZ ! 🥶', true] //Ini buat kamu!
+  ['628981614707'], //Owner1
+  ['628981614707'], //Owner2
+  ['628981614707'], //Owner3
+  ['628981614707', 'MFX ! 🥶', true] //Ini buat kamu!
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -111,13 +111,13 @@ global.APIKeys = { // APIKey Here
 
 /*⫘⫘⫘⫘ WATERMAK ⫘⫘⫘⫘⫘*/
 //GANTI JANGAN NGASAL.
-global.wm = '                「 Fangz BOT ⁩இ 」' //Wm
-global.wm2 = '➳༻❀Fangz - Ganz❁ུ۪۪.;:୭̥.' //Name Wm
+global.wm = '                「 MFC BOT ⁩இ 」' //Wm
+global.wm2 = '➳༻❀MFX - Ganz❁ུ۪۪.;:୭̥.' //Name Wm
 global.wm3 = '⫹⫺ Fangz 𝗕𝗢𝗧'
 global.botdate = `⫹⫺ 𝗗𝗮𝘁𝗲: ${week} ${date}`
 global.bottime = `𝗧 𝗜 𝗠 𝗘 : ${wktuwib}`
-global.titlebot = '𝗬𝗧: FANGZ BOT' //Titlenya
-global.author = '                ✧ Fangz BOT⁩ ✧' //Wm author
+global.titlebot = 'MFC BOT' //Titlenya
+global.author = '                ✧ MFC BOT⁩ ✧' //Wm author
 
 
 /*⫘⫘⫘⫘ LOGO ⫘⫘⫘⫘⫘*/
