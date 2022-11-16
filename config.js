5e74f6901a02de11a6a5e1a3bd79e67720c1f8f5
yo564 <118075541+yo564@users.noreply.github.com>
2022-11-17 02:21:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -51,18 +51,18 @@ global.ppulsa2 = '' //Pulsa, Kalau nomor cuma 1, yg ini kasih tanda -
 global.psaweria = '' //Saweria
 
 /*⫘⫘⫘⫘ GLOBAL NOMOR ⫘⫘⫘⫘⫘*/
-global.nomorbot = '6283831242402' //Nomor Bot mu
-global.nomorown = '6283814346766' //Nomormu
+global.nomorbot = '6283814346766' //Nomor Bot mu
+global.nomorown = '6283831242402' //Nomormu
 global.namebot = 'MFC BOTZ' //Nama bot mu
 global.nameown = 'MFX' //Nama mu
 
 
 /*⫘⫘⫘⫘ STAFF ⫘⫘⫘⫘⫘*/
 global.owner = [
-  ['6283814346766'], //Owner1
-  ['6283814346766'], //Owner2
+  ['6283831242402'], //Owner1
+  ['6283831242402'], //Owner2
   ['62895613101691'], //Owner3
-  ['6283814346766', 'YUDI XD ! 🥶', true] //Ini buat kamu!
+  ['6283831242402', 'YUDI XD ! 🥶', true] //Ini buat kamu!
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
